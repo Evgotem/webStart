2fabec866254dad1f449a25a12eb4e1370466737
Evgotem <57010853+Evgotem@users.noreply.github.com>
2020-02-18 17:01:32 +0400

    Сделал так, что-бы модальное окно само закрывалось через 5 секунд после открытия с помощью функции setTimeout.

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,12 @@ var close = document.querySelector('#close');
 
 button.addEventListener('click', function() {
 	modal.classList.add('modal_active');
+	setTimeout(function() {
+  document.getElementById('modal').classList.remove('modal_active');
+}, 5000);
 });
 
 close.addEventListener('click', function() {
 	modal.classList.remove('modal_active');
 });
+
