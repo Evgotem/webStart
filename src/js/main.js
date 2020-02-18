79a44b4db3bd7cb31843f3d285f4bc7129ed383f
Evgotem <57010853+Evgotem@users.noreply.github.com>
2020-02-18 20:17:05 +0400

    Подключил jQuery, код для модального окна переписал на jQuery, сверстал стрелку в левом нижнем углу, написал код на jQuery для прокрутки экрана вверх при нажатии на эту кнопку (main.js).

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,5 @@
-var button = document.querySelector('#button');
-var modal = document.querySelector('#modal');
-var close = document.querySelector('#close');
-
-button.addEventListener('click', function() {
-	modal.classList.add('modal_active');
-	setTimeout(function() {
-  document.getElementById('modal').classList.remove('modal_active');
-}, 5000);
+$(document).ready(function() {
+	$('#arrow-start').click(function() {
+		$('html').animate({scrollTop:0}, 1000);
+	});
 });
-
-close.addEventListener('click', function() {
-	modal.classList.remove('modal_active');
-});
-
