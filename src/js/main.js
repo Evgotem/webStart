304de7c56907ce842f10c3a5587dd5eec76a99ae
Evgotem <57010853+Evgotem@users.noreply.github.com>
2020-02-20 20:56:09 +0400

    переписал скрипт  для стрелки прокрутки экрана, что-бы она плавно появлялась при скролле ниже чем 200px, а на экранах меньше 768px сделал, что-бы она появлялась справа.

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,13 @@ $(document).ready(function() {
 	$('#arrow-start').click(function() {
 		$('html').animate({scrollTop:0}, 1000);
 	});
+	$(window).scroll(function(){
+		if($(window).scrollTop()>200){
+            $('#arrow-start').addClass('show');
+        }else{
+           $('#arrow-start').removeClass('show');
+        }
+	});
 });
+
 
